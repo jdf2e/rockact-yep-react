27e657412fc5ccad2004d6f3f9b05e490bd2ba26
zhhyang <zhhyang0404@163.com>
2020-10-16 09:53:45 +0800

    bump yep-react@1.0.1

--- src/pages/app.tsx
+++ src/pages/app.tsx
@@ -4,10 +4,12 @@ import * as ReactDOM from "react-dom";
 import "./app.scss";
 
 import Welcome from "../components/Welcome";
+import {Loading} from "@jdcfe/yep-react";
 
 ReactDOM.render(
-  <div className="App">
-    <Welcome message="Hello World!" />
-  </div>,
-  document.getElementById("app") as HTMLElement
+    <div className="App">
+        <Loading/>
+        <Welcome message="Hello World!"/>
+    </div>,
+    document.getElementById("app") as HTMLElement
 );
